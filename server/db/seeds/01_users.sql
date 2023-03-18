287eebffc6fc1cc5219f28ed96cdb7d291d3ac32
Jason Divinagracia <jmarkd04@gmail.com>
2023-03-18 17:03:27 -0400

    Update seeds

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,6 +1,6 @@
-INSERT INTO users (username, email, password, first_name, last_name, profile_picture, bio) VALUES
-  ('john_doe', 'john_doe@example.com', 'password', 'John', 'Doe', 'https://example.com/profiles/john_doe.jpg', 'I love dogs!'),
-  ('jane_smith', 'jane_smith@example.com', 'password', 'Jane', 'Smith', 'https://example.com/profiles/jane_smith.jpg', 'Dog mom of three!'),
-  ('bob_johnson', 'bob_johnson@example.com', 'password', 'Bob', 'Johnson', 'https://example.com/profiles/bob_johnson.jpg', 'Dog enthusiast and trainer.'),
-  ('emily_wilson', 'emily_wilson@example.com', 'password', 'Emily', 'Wilson', 'https://example.com/profiles/emily_wilson.jpg', 'I rescue dogs and find them their forever homes.'),
-  ('will_smith', 'will_smith@example.com', 'password', 'Will', 'Smith', 'https://example.com/profiles/will_smith.jpg', 'I love dogs as much as I love acting!');
+INSERT INTO users (username, email, password, first_name, last_name, profile_picture, bio) VALUES 
+('lovedogs23', 'lovedogs23@gmail.com', 'password', 'Sarah', 'Jones', 'https://i.imgur.com/O7tPFPG.jpg', 'Dog lover and avid walker. Miss my best friend so much.'),
+('dogmomma', 'dogmomma@hotmail.com', 'password', 'Emily', 'Lee', 'https://i.imgur.com/6Q1R6xg.jpg', 'My heart aches every day for my sweet boy.'),
+('puppers4ever', 'puppers4ever@yahoo.com', 'password', 'Ryan', 'Chen', 'https://i.imgur.com/zsU6cKj.jpg', 'My dogs are my life, even in death.'),
+('ruffrider', 'ruffrider@gmail.com', 'password', 'David', 'Nguyen', 'https://i.imgur.com/UPnS2S1.jpg', 'My dog was the coolest dude. Miss him a lot.'),
+('barkingmad', 'barkingmad@gmail.com', 'password', 'Grace', 'Davis', 'https://i.imgur.com/jqvDlty.jpg', 'Lost my furry soulmate. This app is helping me cope.');
--- server/db/seeds/02_dogs.sql
+++ server/db/seeds/02_dogs.sql
@@ -1,6 +1,6 @@
 INSERT INTO dogs (name, description, profile_picture, date_birth, date_passing) VALUES
-  ('Buddy', 'A friendly and loyal companion.', 'https://example.com/dogs/buddy.jpg', '2015-02-14', NULL),
-  ('Lassie', 'A highly intelligent and loyal dog.', 'https://example.com/dogs/lassie.jpg', '2018-07-23', NULL),
-  ('Max', 'A scent hound known for his excellent tracking abilities.', 'https://example.com/dogs/max.jpg', '2019-01-01', NULL),
-  ('Ace', 'A highly energetic breed often used for hunting.', 'https://example.com/dogs/ace.jpg', '2016-11-11', NULL),
-  ('Bella', 'A terrier mix with a lot of personality.', 'https://example.com/dogs/bella.jpg', '2017-05-20', NULL);
+  ('Buddy', 'A friendly and loyal companion.', 'https://example.com/dogs/buddy.jpg', '2010-01-01', '2022-02-14'),
+  ('Lassie', 'A highly intelligent and loyal dog.', 'https://example.com/dogs/lassie.jpg', '2014-05-10', '2022-01-02'),
+  ('Max', 'A scent hound known for his excellent tracking abilities.', 'https://example.com/dogs/max.jpg', '2012-09-20', '2021-11-18'),
+  ('Ace', 'A highly energetic breed often used for hunting.', 'https://example.com/dogs/ace.jpg', '2013-12-25', '2022-03-07'),
+  ('Bella', 'A terrier mix with a lot of personality.', 'https://example.com/dogs/bella.jpg', '2011-06-15', '2021-09-30');
