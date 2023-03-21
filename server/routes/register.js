85594d0906708a79cbd0c214615450f8e0c78715
Denny Doan <denny-333@hotmail.com>
2023-03-20 22:21:25 -0400

    Can register new users, still working on edge cases

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,45 +1,29 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const pool = require('../configs/db.config.js');
+const userQueries = require('../db/queries/users');
 const router = express.Router();
 
 // checks registration details once submitted
 router.post('/', (req, res) => {
   const { username, email, firstName, lastName, password, passwordConfirmation } = req.body;
 
-  // checks if username exists
-  pool.query('SELECT id FROM users WHERE username = $1', [username])
-    .then((usernameExists) => {
-      if (usernameExists.rowCount > 0) {
-        return res.status(400).json({ error: 'Username already taken' });
+  userQueries
+    .userExists(username, email)
+    .then((userExists) => {
+      if (userExists && userExists.email === email) {
+        return res.status(400).send({ error: 'Email already exists' });
+      } else if (userExists && userExists.username === username) {
+        return res.status(400).send({ error: 'Username already exists' });
+      } else {
+        return userQueries.addUser(username, email, password, firstName, lastName);
       }
-      // checks if email already exists
-      return pool.query('SELECT id FROM users WHERE email = $1', [email]);
     })
-    .then((emailExists) => {
-      if (emailExists.rowCount > 0) {
-        return res.status(400).json({ error: 'Email already registered' });
-      }
-
-      // checks password/confirmation match
-      if (password !== passwordConfirmation) {
-        return res.status(400).json({ error: 'Passwords do not match' });
-      }
-
-      // encrypts password and stores in db
-      return bcrypt.hash(password, 10)
-        .then((hashedPassword) => {
-          return pool.query('INSERT INTO users (username, email, password, first_name, last_name) VALUES ($1, $2, $3, $4, $5) RETURNING *', [username, email, hashedPassword, firstName, lastName]);
-        })
-        .then((result) => {
-          const newUser = result.rows[0];
-          console.log('Successfully created user:', result.rows[0]);
-          return res.status(200).json({ message: 'User registered successfully' });
-        });
+    .then((user) => {
+      console.log('New user created:', user.rows[0]);
+      return res.status(201).send({ message: 'User created successfully' });
     })
     .catch((error) => {
-      console.error('Error registering user:', error);
-      return res.status(500).json({ error: 'Failed to register user' });
+      console.error(error);
+      return res.status(500).send({ error: 'Server error' });
     });
 });
 
