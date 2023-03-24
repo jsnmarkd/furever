d487d5dfd08984596cf38a2e99437a3681b52c24
Denny Doan <denny-333@hotmail.com>
2023-03-24 14:11:17 -0400

    Adjusted db to have dog media in relation to dog id, displaying username instead of email

--- server/db/seeds/03_dog_media.sql
+++ server/db/seeds/03_dog_media.sql
@@ -1,6 +1,6 @@
-INSERT INTO dog_media (media_picture, media_video, media_description) VALUES
-  ('https://placedog.net/640/480?id=31', 'https://www.shutterstock.com/shutterstock/videos/1068515015/preview/stock-footage-golden-retriever-eating-dog-food-from-metal-bowl-concept-of-online-shop-delivery-for-pets.webm', 'After a long walk at the #park'),
-  ('https://placedog.net/640/480?id=4', 'https://www.shutterstock.com/shutterstock/videos/1017130072/preview/stock-footage-funny-puppy-eat-from-a-bowl.webm', 'After eating all the #treats'),
-  ('https://placedog.net/640/480?id=6', 'https://www.shutterstock.com/shutterstock/videos/1031036624/preview/stock-footage-beagle-puppy-trying-to-come-down.webm', '#play time'),
-  ('https://placedog.net/640/480?id=30', 'https://www.shutterstock.com/shutterstock/videos/1058381281/preview/stock-footage-jack-russell-terrier-looks-out-the-open-window-of-the-car-close-up-slow-motion.webm', 'Shades on! #grooming'),
-  ('https://placedog.net/640/480?id=32', 'https://www.shutterstock.com/shutterstock/videos/1057720540/preview/stock-footage-close-up-shot-of-young-bearded-man-hugging-and-kissing-his-cute-friend-brown-basenji-pure-breed-dog.webm', '#play #park');
+INSERT INTO dog_media (dog_id, media_picture, media_video, media_description) VALUES
+  (1, 'https://placedog.net/640/480?id=31', 'https://www.shutterstock.com/shutterstock/videos/1068515015/preview/stock-footage-golden-retriever-eating-dog-food-from-metal-bowl-concept-of-online-shop-delivery-for-pets.webm', 'After a long walk at the #park'),
+  (2, 'https://placedog.net/640/480?id=4', 'https://www.shutterstock.com/shutterstock/videos/1017130072/preview/stock-footage-funny-puppy-eat-from-a-bowl.webm', 'After eating all the #treats'),
+  (3, 'https://placedog.net/640/480?id=6', 'https://www.shutterstock.com/shutterstock/videos/1031036624/preview/stock-footage-beagle-puppy-trying-to-come-down.webm', '#play time'),
+  (4, 'https://placedog.net/640/480?id=30', 'https://www.shutterstock.com/shutterstock/videos/1058381281/preview/stock-footage-jack-russell-terrier-looks-out-the-open-window-of-the-car-close-up-slow-motion.webm', 'Shades on! #grooming'),
+  (5, 'https://placedog.net/640/480?id=32', 'https://www.shutterstock.com/shutterstock/videos/1057720540/preview/stock-footage-close-up-shot-of-young-bearded-man-hugging-and-kissing-his-cute-friend-brown-basenji-pure-breed-dog.webm', '#play #park');
