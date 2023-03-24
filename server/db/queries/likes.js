4f3db29bdcceedf7dac14b8a70655dc8f52169bb
Jason Divinagracia <jmarkd04@gmail.com>
2023-03-24 16:06:39 -0400

    Implement like feature

--- server/db/queries/likes.js
+++ server/db/queries/likes.js
@@ -6,4 +6,39 @@ const getAllLikes = () => {
   });
 };
 
-module.exports = { getAllLikes };
+const likedByUser = (user_id, content_id) => {
+  return db.query(
+    `SELECT COUNT(*) FROM likes WHERE user_id = $1 AND content_id = $2`,
+    [user_id, content_id]
+  ).then((data) => {
+    return data.rows;
+  });
+};
+
+const addLike = (user_id, content_id) => {
+  const sql = `INSERT INTO likes (user_id, content_id)
+  VALUES ($1, $2)
+  RETURNING *;`;
+  return db
+    .query(sql, [user_id, content_id])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+const deleteLike = (user_id, content_id) => {
+  const sql = `DELETE FROM likes WHERE user_id = $1 AND content_id = $2;`;
+  return db
+    .query(sql, [user_id, content_id])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+module.exports = { getAllLikes, addLike, likedByUser, deleteLike };
--- server/routes/likes.js
+++ server/routes/likes.js
@@ -9,4 +9,47 @@ router.get("/", (req, res) => {
   });
 });
 
+// Example
+// http://localhost:8080/liked?user_id=1&content_id=1
+
+router.get('/liked', (req, res) => {
+  const userId = req.query.user_id;
+  const contentId = req.query.content_id;
+
+  likes.likedByUser(userId, contentId)
+    .then((data) => {
+      const count = parseInt(data[0].count);
+      res.json(count > 0); //returns true if count > 0 else false
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message })
+    })
+})
+
+router.post("/", (req, res) => {
+  const userId = req.body.user_id;
+  const contentId = req.body.content_id;
+
+  likes.addLike(userId, contentId)
+    .then((like) => {
+      res.status(200).json(like);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.delete("/", (req, res) => {
+  const userId = req.body.user_id;
+  const contentId = req.body.content_id;
+
+  likes.deleteLike(userId, contentId)
+    .then((like) => {
+      res.status(200).json(like);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
 module.exports = router;
